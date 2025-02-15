14c1b131715b8dfb61adf492b66ff3c3f8de7b6a
farjana96-dipa <fparul32@gmail.com>
2025-02-15 22:57:00 +0600

    mega menu

--- src/components/header/nav/nav.js
+++ src/components/header/nav/nav.js
@@ -5,15 +5,15 @@ import Button from 'react-bootstrap/Button'
 import {Link} from 'react-router-dom'
 const nav = () => {
   return (
-    <div className='nav my-3'>
+    <div className='nav my-3 position-relative'>
         <div className='container-fluid'>
          <div className='row'>
-            <div className='col-sm-2 part1 d-flex align-items-center'>
+            <div className='col-sm-2 part1 d-flex align-items-center position-relative'>
             
             <button className='btn btn-success'><i class="fa-solid fa-list"></i>Browse All Categories <i class="fa-solid fa-angle-down sicon"></i></button>
            
             </div>
-            <div className='col-sm-8 part2 d-flex align-items-center'>
+            <div className='col-sm-8 part2 d-flex align-items-center position-static'>
               <nav className=''>
                 <ul className='list list-inline mb-3'>
                   <li className='list-inline-item'>
@@ -56,6 +56,56 @@ const nav = () => {
 
                   <li className='list-inline-item'>
                     <Button><Link>Mega Menu</Link></Button>
+
+                    <div className='dropdown_menu mega_menu'>
+                      <div className='row'>
+                        <div className='col-md-4 col-sm-12'>
+                          <ul>
+                            <li><Link>About Us</Link></li>
+                            <li><Link>Contact Us</Link></li>
+                            <li><Link>My Account</Link></li>
+                            <li><Link>Login</Link></li>
+                            <li><Link>Register</Link></li>
+                            <li><Link>Forget Password</Link></li>
+                            <li><Link>Reset Password</Link></li>
+                            <li><Link>Purchase Guide</Link></li>
+                            <li><Link>Privacy Policy</Link></li>
+                            <li><Link>Terms & Conditions</Link></li>
+                          </ul>
+                        </div>
+
+                        <div className='col-md-4 col-sm-12'>
+                        <ul>
+                            <li><Link>About Us</Link></li>
+                            <li><Link>Contact Us</Link></li>
+                            <li><Link>My Account</Link></li>
+                            <li><Link>Login</Link></li>
+                            <li><Link>Register</Link></li>
+                            <li><Link>Forget Password</Link></li>
+                            <li><Link>Reset Password</Link></li>
+                            <li><Link>Purchase Guide</Link></li>
+                            <li><Link>Privacy Policy</Link></li>
+                            <li><Link>Terms & Conditions</Link></li>
+                          </ul>
+                        </div>
+
+                        <div className='col-md-4 col-sm-12'>
+                        <ul>
+                            <li><Link>About Us</Link></li>
+                            <li><Link>Contact Us</Link></li>
+                            <li><Link>My Account</Link></li>
+                            <li><Link>Login</Link></li>
+                            <li><Link>Register</Link></li>
+                            <li><Link>Forget Password</Link></li>
+                            <li><Link>Reset Password</Link></li>
+                            <li><Link>Purchase Guide</Link></li>
+                            <li><Link>Privacy Policy</Link></li>
+                            <li><Link>Terms & Conditions</Link></li>
+                          </ul>
+                        </div>
+                      </div>
+                       
+                      </div>
                   </li>
 
                   <li className='list-inline-item'>
@@ -64,7 +114,7 @@ const nav = () => {
                 </ul>
               </nav>
             </div>
-            <div className='col-sm-2 part3 d-flex align-items-center' >
+            <div className='col-sm-2 part3 d-flex align-items-center position-static' >
                 <div className='phoneNumber d-flex'>
                       <i class="fa-solid fa-phone"></i> 
                       <div className='secondPart'>
